1bd6f35a3bdc4054706b8ff7eedfb2c37160978b
Tom_Court <tomcourt94@icloud.com>
2023-08-10 16:36:52 +0100

    animation

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import { motion } from "framer-motion";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonData, setPokemonData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchPokemon = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
       );
@@ -15,6 +18,8 @@ function App() {
     } catch (error) {
       console.error("Error fetching Pokémon:", error);
       setPokemonData(null);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -22,67 +27,90 @@ function App() {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
+  useEffect(() => {
+    setPokemonData(null);
+  }, [pokemonName]);
+
   return (
-    <div className="Pokemon-App">
-      <h1>Pokémon Pokédex</h1>
-      <input
-        type="text"
-        placeholder="Enter Pokémon name or ID"
-        value={pokemonName}
-        onChange={(e) => setPokemonName(e.target.value)}
-      />
-      <button onClick={fetchPokemon}>Search</button>
+    <div className="Pokemon-App container">
+      <h1 className="text-center mt-5">Pokémon Pokédex</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter Pokémon name or ID"
+          value={pokemonName}
+          onChange={(e) => setPokemonName(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={fetchPokemon}>
+          Search
+        </button>
+      </div>
 
       {pokemonData && (
-        <div className="total-data">
-          <h2 className="name">{capitalizeFirstLetter(pokemonData.name)}</h2>
-          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        <motion.div
+          key={pokemonData.id} 
+          className="total-data mt-5"
+        >
+          {/* Image Animation */}
+                   <h2 className="name">{capitalizeFirstLetter(pokemonData.name)}</h2>
+            
+            <motion.img
+            src={pokemonData.sprites.front_default}
+            alt={pokemonData.name}
+            className="pokemon-image"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+          />
 
-          <div className="details">
-            <h3>Details:</h3>
-            <p>Height: {pokemonData.height / 10} meters</p>
-            <p>Weight: {pokemonData.weight / 10} kg</p>
-          </div>
 
-          <div className="types">
-            <h3>Types:</h3>
-            <p>
-              {pokemonData.types
-                .map((type) => capitalizeFirstLetter(type.type.name))
-                .join(", ")}
-            </p>
-          </div>
+          {/* Delayed Data */}
+          {!isLoading && (
+            <motion.div
+              className="details-container"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1 }}
+            >
+              <div className="types">
+                <h3>Types:</h3>
+                <p>
+                  {pokemonData.types
+                    .map((type) => capitalizeFirstLetter(type.type.name))
+                    .join(", ")}
+                </p>
+              </div>
 
-          <div className="abilities">
-            <h3>Abilities:</h3>
-            <p>
-              {pokemonData.abilities
-                .map((ability) => capitalizeFirstLetter(ability.ability.name))
-                .join(", ")}
-            </p>
-          </div>
+              <div className="abilities">
+                <h3>Abilities:</h3>
+                <p>
+                  {pokemonData.abilities
+                    .map((ability) =>
+                      capitalizeFirstLetter(ability.ability.name)
+                    )
+                    .join(", ")}
+                </p>
+              </div>
 
-          <div className="stats">
-            <h3>Stats:</h3>
-            {pokemonData.stats.map((stat) => (
-              <p key={stat.stat.name}>
-                {capitalizeFirstLetter(stat.stat.name)}: {stat.base_stat}
-              </p>
-            ))}
-          </div>
+              <div className="stats">
+                <h3>Stats:</h3>
+                <div className="bulletpoints">
+                {pokemonData.stats.map((stat) => (
+                  <p key={stat.stat.name}>
+                    {capitalizeFirstLetter(stat.stat.name)}: {stat.base_stat}
+                  </p>
+                ))}
+                </div>
+              </div>
 
-          {/* <div className="moves">
-          <h3>Moves:</h3>
-          <ul>
-            {pokemonData.moves.map((move) => (
-              <li key={move.move.name}>
-                {capitalizeFirstLetter(move.move.name)}
-              </li>
-            ))}
-          </ul>
-        </div> */}
-        
-        </div>
+              <div className="details">
+                <h3>Details:</h3>
+                <p>Height: {pokemonData.height / 10} meters</p>
+                <p>Weight: {pokemonData.weight / 10} kg</p>
+              </div>
+            </motion.div>
+          )}
+        </motion.div>
       )}
     </div>
   );
--- pokedex/src/App.test.js
+++ pokedex/src/App.test.js
@@ -6,3 +6,17 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+ {
+   /* <div className="moves">
+          <h3>Moves:</h3>
+          <ul>
+            {pokemonData.moves.map((move) => (
+              <li key={move.move.name}>
+                {capitalizeFirstLetter(move.move.name)}
+              </li>
+            ))}
+          </ul>
+        </div> */
+ }
